d8dbe972cb24ea1f1875fc699996f1be9828a826
Loic Nageleisen <loic.nageleisen@gmail.com>
2015-11-01 12:51:57 +0100

    style

--- src/load-src.js
+++ src/load-src.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var qs = window.location.search
-	var src = decodeURIComponent(qs.replace('?', '').split('&')[0].split('=')[1]);
-	document.querySelector('#mattermost-remote').setAttribute('src', src);
+    var qs = window.location.search
+    var src = decodeURIComponent(qs.replace('?', '').split('&')[0].split('=')[1]);
+    document.querySelector('#mattermost-remote').setAttribute('src', src);
 });
