766a1a2eab9776ce3c96f68694835b8431844d5c
Chris Jaynes <chris.jaynes.sc8l@statefarm.com>
2015-12-01 11:34:31 -0600

    Fix indents

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ app.on('ready', function() {
   });
 
   mainWindow.webContents.on('will-navigate', function (e) {
-      e.preventDefault();
+    e.preventDefault();
   });
 
   mainWindow.on('closed', function() {
