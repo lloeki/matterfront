3c7d366161c5997187b9024390bedb8e5728491f
Chris Jaynes <chris.jaynes.sc8l@statefarm.com>
2015-12-01 11:15:58 -0600

    add new files that I forgot to stage (whoops)

---
+++ src/window-opener.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var webview = document.getElementById("mattermost-remote");
+  webview.addEventListener('new-window', function(e) {
+     require('shell').openExternal(e.url);
+   });
+});
