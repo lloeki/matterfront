a09f8df829988ec2b17c39c043a9e2eda1ff4d68
Chris Jaynes <chris.jaynes.sc8l@statefarm.com>
2015-12-09 21:34:45 -0600

    Remove connection-observer
    The webviews will handle their own connection events and overlays

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,5 +1,4 @@
 var App = require("./app.jsx");
-var connectionObserver = require("./connection-observer.js");
 var React = require("react");
 var ReactDOM = require("react-dom");
 var teamObserver = require("./team-observer.js");
@@ -9,7 +8,6 @@ var domElement = document.querySelector("#app");
 var reactElement = React.createElement(App);
 ReactDOM.render(reactElement, domElement);
 
-connectionObserver.start();
 teamObserver.start();
 
 //All of this code will be modularized properly and moved into React components
