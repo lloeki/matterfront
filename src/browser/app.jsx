857359ee3ca5f438e71e61f108757c4e58cc32f1
Chris Jaynes <chris.jaynes.sc8l@statefarm.com>
2015-12-09 21:41:20 -0600

    switch app to use new buttons and views

--- src/browser/app.jsx
+++ src/browser/app.jsx
@@ -1,6 +1,7 @@
 var appState = require("./app-state.js");
 var React = require("react");
-var TeamWebview = require("./team-webview.jsx");
+var TeamButtons = require("./team-buttons.jsx");
+var TeamViews = require("./team-views.jsx");
 
 var App = React.createClass({
   componentDidMount: function(){
@@ -12,8 +13,9 @@ var App = React.createClass({
   },
   render: function(){
     return (
-      <div>
-        <TeamWebview {...this.state}/>
+      <div className="app">
+        <TeamButtons {...this.state} />
+        <TeamViews {...this.state} />
       </div>
     );
   }
